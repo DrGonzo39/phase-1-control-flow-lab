92419e3acf0784ef0a4b75a3339d3e5a6f4cd440
Arthur Benware <artpolitic91@gmail.com>
2022-08-31 21:11:25 -0400

    commit tree 21f27f004c8b2c6394cf02c68be9e058b4fd1366

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet === 199) {
+    return 'This one is on me!'
+  }
+  else if (feet === 1500) {
+    return 'That will be twenty bucks.'
+  }
+  else if (feet === 2001) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else if (feet === 2501) {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC'? 'Ok, sounds good.': 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+    case 'generous' :
+      return 'Thank you so much.'
+    case 'not as generous' :
+      return 'Thank you.'
+    default :
+      return 'Bye.'
+      
+
+
+  }
 }
